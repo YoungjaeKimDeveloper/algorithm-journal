5e08598fef69e98353f005f289ebc202fd6ac421
Youngjae <2015fromjeki@gmail.com>
2025-04-07 11:44:49 +1000

    D2 Linked List Data Structure

---
+++ D2_LinkedList/07-04-2025-linked-List.md
@@ -0,0 +1,25 @@
+Today's Notes
+
+1. Array VS LinkedList
+    A LinkedList consists of a head and a tail.
+
+    Each node points to the next node.
+
+    The last node points to null, indicating the end of the list.
+
+    A linked list is not stored in contiguous memory; each node can be stored anywhere in memory.
+
+2. Big O Notation
+    O(n) - For example, removing the last element(pop) in a singly linked list requires the list.
+    
+    O(1) - Some operations like addingto the end(with a tail reference) can be done in constant time.
+
+    Choosing Data Structures
+    Always consider when a data structure is useful and which one is optimal for the situation.
+
+    Node as an Object
+        A node is an object in Javascript
+
+    JavaScript Object = a flexible data struture for grouping related data and behavior using key-value pairs.
+
+
---
+++ D2_LinkedList/LinkedList.js
@@ -0,0 +1,79 @@
+{
+    head:{
+        value:11,
+        next:{
+            value:3,
+            next:{
+                value:3,
+                next:{
+                    value:23,
+                    next:{
+                        value:7,
+                        next:{
+                            value:4,
+                            next:null
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Node Constructor  - Node Class
+class Node{
+    constructor(value){
+        this.value = value;  // node value
+        this.next = null; // initially, next is null because the node doesn't point to anything.
+    }
+}
+// LinkedList: a fundamental data structure where each node points to the next
+// when there is only one node, pointer points[refer] to head and tail.
+class LinkedList {
+    constructor(value){
+        const newNode = new Node(value);
+        this.head = newNode; // the first node in the list
+        this.tail = this.head; //the last node in the list
+        this.length = 1 // the number of node
+    }
+}
+
+// Value = The data node has.
+// next = next node
+// next = null. It means it is the last node
+
+// Create one one = Node Head
+const head = new Node(11);
+// Pont the next node tail = null
+head.next = new Node(3);
+// Pont the next node tail = null
+head.next.next = new Node(23);
+// Pont the next node tail = null
+head.next.next.next = new Node(7);
+// Pont the next node tail = null
+head.next.next.next.next = new Node(4);
+
+// Linked List - always when I created new instance: we need a "new" keywrod 
+// "New" keyword calls a constructor
+let myLinkedList = new LinkedList(4);
+
+// Value method
+push(value) {
+    const newNode = new Node(value);
+    // when there is only one node.
+    if(!this.head){
+        this,head = newNode;
+        this.tail = newNode
+    } else{
+        // the next node is new node
+        this.tail.next = newNode
+        // tail is also new Node
+        this.tail = newNode
+    }
+    this.length++
+    // meaning of "this" return entire of LinkedList
+    return this 
+}
+
+
+
