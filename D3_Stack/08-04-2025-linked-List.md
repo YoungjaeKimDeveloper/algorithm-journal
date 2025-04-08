56c5d291633e5c57cd513ee8cf33eca369e2f956
Youngjae <2015fromjeki@gmail.com>
2025-04-08 14:09:19 +1000

    Feat: Stack Data Structure

---
+++ D3_Stack/08-04-2025-linked-List.md
@@ -0,0 +1,15 @@
+Queue works like a tennis ball in a container.
+
+FIFO (First In, First Out)
+
+    Stack works like a linked stack (e.g., Gmail, Instagram, YouTube, Facebook).
+
+    LIFO (Last In, First Out)
+
+    Stack: Array Data Structure
+
+    Time Complexity:
+
+    Pop = O(n) – Remove from the end of the array.
+
+    Push = O(1) – Add to the end of the array.
---
+++ D3_Stack/Stack.js
@@ -0,0 +1,50 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+// 기본의 Node를 바탕으로 Stack Data Structure를 만들어줌
+class Stack {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.top = newNode;
+    this.length = 1;
+  }
+}
+
+push(value) {
+    const newNode= new Node(value);
+    if(this.length == 0){
+        this.top = newNode;
+    }else{
+        newNode.next = this.top;
+        this.top = newNode;
+    }
+    this.length++
+    // return entire of the stack
+    return this;
+}
+
+pop() {
+    if(this.length ===0) return undefined;
+    // it points the top element of stack
+    let temp = this.top;
+    this.top = this.top.next
+    temp.next = null;
+    this.length--;
+    return temp;
+}
+
+/*  
+    Unshift -> Add new elements at the front of the array
+    const array1 = [1, 2, 3];
+
+    console.log(array1.unshift(4, 5));
+    Expected output: 5
+
+    console.log(array1);
+    Expected output: Array [4, 5, 1, 2, 3]
+
+
+*/
